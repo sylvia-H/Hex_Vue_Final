54e14d5e867a4664d51073e2536151acfd1f42c7
sylvia-H <sylvie11gaia@gmail.com>
2022-04-07 21:18:27 +0800

    revise myFavorites layout

--- src/views/Front/FavCollection.vue
+++ src/views/Front/FavCollection.vue
@@ -3,9 +3,10 @@
     <h2 class="text-center">我的收藏</h2>
     <hr class="my-6" />
     <div class="row text-center">
-      <div>
-        <!-- 我的收藏列表 -->
-        <table v-if="Object.keys(collection).length" class="table table-hover table-responsive">
+      <div class="col-12 table-responsive">
+        <!-- 我的收藏列表 lg 以上電腦版本 -->
+        <table v-if="Object.keys(collection).length"
+         class="table table-hover | d-none d-lg-block">
           <thead>
             <tr>
               <th scope="col">品名</th>
@@ -55,6 +56,151 @@
               </td>
               <td class="align-middle">
                 {{ (collection[item].protein * collection[item].number) / 100 }} g
+              </td>
+              <td class="align-middle">
+                <router-link :to="`/product/${collection[item].id}`">
+                  <i class="bi bi-zoom-in"></i>
+                </router-link>
+              </td>
+              <td class="align-middle">
+                <button
+                  @click="addCart(item)"
+                  :disabled="item === is_loadingItem"
+                  type="button"
+                  class="btn btn-dark d-flex align-items-center"
+                >
+                  <div
+                    v-if="item === is_loadingItem"
+                    class="spinner-border text-warning"
+                    role="status"
+                  >
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                  <div class="d-flex" v-else>
+                    <i class="bi bi-cart3 me-1"></i>
+                    <span class="d-none d-lg-block"> 想買 </span>
+                  </div>
+                </button>
+              </td>
+              <td class="align-middle">
+                <button
+                  type="button"
+                  @click="addCollection(collection[item])"
+                  class="btn btn-outline-danger d-flex align-items-center"
+                  :class="{ active: collection[item] }"
+                >
+                  <i class="bi bi-heart"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- 我的收藏列表 md 平板版本 -->
+        <table v-if="Object.keys(collection).length"
+         class="table table-hover | d-none d-md-block d-lg-none">
+          <thead>
+            <tr>
+              <th scope="col">品名</th>
+              <th scope="col">類別</th>
+              <th scope="col">內含份量</th>
+              <th scope="col">總熱量</th>
+              <th scope="col">查看商品細節</th>
+              <th scope="col">加入購物車</th>
+              <th scope="col">收藏中</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in Object.keys(collection)" :key="item">
+              <th scope="row" class="align-middle">
+                <span class="badge bg-primary mb-2">
+                  {{ collection[item].category }}
+                </span>
+                <br />
+                {{ collection[item].title }}
+              </th>
+              <td class="align-middle">
+                <img
+                  style="max-width: 100px"
+                  :src="collection[item].imageUrl"
+                  :alt="collection[item].title"
+                />
+              </td>
+              <td class="align-middle">
+                {{ collection[item].number }} {{ collection[item].unit }}
+              </td>
+              <td class="align-middle">
+                {{ (collection[item].calorie * collection[item].number) / 100 }}
+                kcal
+              </td>
+              <td class="align-middle">
+                <router-link :to="`/product/${collection[item].id}`">
+                  <i class="bi bi-zoom-in"></i>
+                </router-link>
+              </td>
+              <td class="align-middle">
+                <button
+                  @click="addCart(item)"
+                  :disabled="item === is_loadingItem"
+                  type="button"
+                  class="btn btn-dark d-flex align-items-center"
+                >
+                  <div
+                    v-if="item === is_loadingItem"
+                    class="spinner-border text-warning"
+                    role="status"
+                  >
+                    <span class="visually-hidden">Loading...</span>
+                  </div>
+                  <div class="d-flex" v-else>
+                    <i class="bi bi-cart3 me-1"></i>
+                    <span class="d-none d-lg-block"> 想買 </span>
+                  </div>
+                </button>
+              </td>
+              <td class="align-middle">
+                <button
+                  type="button"
+                  @click="addCollection(collection[item])"
+                  class="btn btn-outline-danger d-flex align-items-center"
+                  :class="{ active: collection[item] }"
+                >
+                  <i class="bi bi-heart"></i>
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <!-- 我的收藏列表 md 以下手機版本 -->
+        <table v-if="Object.keys(collection).length"
+         class="table table-hover | d-block d-md-none">
+          <thead>
+            <tr>
+              <th scope="col">品名</th>
+              <th scope="col">類別</th>
+              <th scope="col">內含份量</th>
+              <th scope="col">查看商品細節</th>
+              <th scope="col">加入購物車</th>
+              <th scope="col">收藏中</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in Object.keys(collection)" :key="item">
+              <th scope="row" class="align-middle">
+                <span class="badge bg-primary mb-2">
+                  {{ collection[item].category }}
+                </span>
+                <br />
+                {{ collection[item].title }}
+              </th>
+              <td class="align-middle">
+                <img
+                  style="max-width: 100px"
+                  :src="collection[item].imageUrl"
+                  :alt="collection[item].title"
+                />
+              </td>
+              <td class="align-middle">
+                {{ collection[item].number }} {{ collection[item].unit }}
               </td>
               <td class="align-middle">
                 <router-link :to="`/product/${collection[item].id}`">
