33c3ff56c9ffd5911fa5f836b75affd38c4518b2
sylvia-H <sylvie11gaia@gmail.com>
2022-04-27 17:41:32 +0800

    revise shipping page color

--- src/views/Front/FShippingView.vue
+++ src/views/Front/FShippingView.vue
@@ -16,7 +16,7 @@
         <h4 class="text-green1 fw-bold mb-4">
           量身打造專屬菜單
         </h4>
-        <p class="text-start text-green2">
+        <p class="text-start text-gray">
           您可以前往「食前好思」頁面，深入了解您目前的身體狀態：BMI 指數、BMR 基礎代謝率與 TDEE 每日總消耗熱量等各種數值，為自己量身打造均衡的營養菜單。
         </p>
       </div>
@@ -30,7 +30,7 @@
         <h4 class="text-green1 fw-bold mb-4">
           盡情選購您喜愛的美食
         </h4>
-        <p class="text-start text-green2">
+        <p class="text-start text-gray">
           請綜合考量您的冰箱容量、食品保存期限以及大致的一週菜單安排，適量選擇產品，需要時再隨時加購！
         </p>
       </div>
@@ -44,7 +44,7 @@
         <h4 class="text-green1 fw-bold mb-4">
           嚴選食材 精緻包裝
         </h4>
-        <p class="text-start text-green2">
+        <p class="text-start text-gray">
           好食遞嚴選優質新鮮食材，依據食材特性進行真空加壓、冷藏冷凍或高溫蒸煮後急凍處理。以維持食材最佳食用鮮度為最高準則。
         </p>
       </div>
@@ -58,7 +58,7 @@
         <h4 class="text-green1 fw-bold mb-4">
           當日烹調 迅速出貨
         </h4>
-        <p class="text-start text-green2">
+        <p class="text-start text-gray">
           除冷凍加壓等經過保鮮處理的食材之外，所有經過二次加工的料理一律當日烹調、當日出貨，務求第一時間送達貴府，讓您擁有最新鮮美好的飲食體驗。
         </p>
       </div>
